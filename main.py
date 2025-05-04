2b013ef880565161add6e22cff4240c94fa31220
Navid Mahdian <mahdian.navid@gmail.com>
2025-05-04 16:40:26 -0700

    Initial FastAPI app

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Hello from local API"}
+
+@app.get("/add")
+def add(a: int, b: int):
+    return {"result": a + b}
+
